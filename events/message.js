bcb374c77917bfca160c557c97b378314e16a9cc
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2020-09-15 20:02:44 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,10 +1,10 @@
 const settings = require('../settings.json');
 module.exports = message => {
-    if (!message.content.startsWith(settings.prefix)) return;
+    if (!message.content.startsWith(settings.prefix.toLowerCase())) return;
     if (message.author.bot) return;
     const client = message.client;
     const args = message.content.split(' ');
-    const command = args.shift().slice(settings.prefix.length);
+    const command = args.shift().slice(settings.prefix.length).toLowerCase();
 
     try {
         let cmdFile = require(`../commands/${command}`);
